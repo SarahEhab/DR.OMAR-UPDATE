0ba4182642cebc7ad2df6807a93287212593fc17
Sara Ehab <saraaehab767@gmail.com>
2024-03-06 01:39:41 +0200

    first commit

---
+++ src/features/books/booksSlice.js
@@ -0,0 +1,54 @@
+// booksSlice.js
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  booksData: [],
+
+  isLoading: false,
+  error: null,
+};
+
+ const getBooks = createAsyncThunk('get/books', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(
+      'https://tsquarehost.info/public/api/Elders/Get');
+      
+    return response.data;
+  } catch (error) {
+    return error
+  }
+});
+
+
+
+
+const booksSlice = createSlice({
+  name: 'booksSlice',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+
+
+      .addCase(getBooks.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getBooks.fulfilled, (state, action) => {
+        state.booksData = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(getBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+	  
+	  
+	  }}
+      );
+export { getBooks };
+
+export default booksSlice.reducer;
